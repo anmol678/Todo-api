5bd455993afb61538c28119d9d2264c343e9693d
anmol678 <anmolj678@gmail.com>
2016-12-23 19:46:08 +0530

    Add POST for /todos

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id: 1,
-	description: 'do laundry',
-	completed: false
-}, {
-	id: 2,
-	description: 'organise music',
-	completed: false
-}, {
-	id: 3,
-	description: 'watch movie',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
+// GET /todos
 app.get('/todos', function (req, res) {
 	res.json(todos);
 });
 
-// app.get('/todos/:id')
+// GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo;
@@ -38,6 +32,16 @@ app.get('/todos/:id', function (req, res) {
 		res.status(404).send();
 	else
 		res.json(matchedTodo);
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	body.id = todoNextId++;
+	todos.push(body);
+
+	res.json(body);
 });
 
 app.listen(PORT, function () {
