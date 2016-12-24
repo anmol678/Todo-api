7ae8b4633b59a28bf0cdb6ff9fed2caa4b453d00
anmol678 <anmolj678@gmail.com>
2016-12-24 17:31:34 +0530

    Add ?completed filter

--- server.js
+++ server.js
@@ -14,9 +14,21 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?query
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed')) {
+		if (queryParams.completed === 'true')
+			filteredTodos = _.where(filteredTodos, {completed: true});
+		else if (queryParams.completed === 'false')
+			filteredTodos = _.where(filteredTodos, {completed: false});
+		else
+			return res.status(400).send();
+	} 
+
+	res.json(filteredTodos);
 });
 
 // GET /todos/:id
