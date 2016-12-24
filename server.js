29c7bf423b9303f86d930453bc23b001a19d4c2f
anmol678 <anmolj678@gmail.com>
2016-12-24 16:58:54 +0530

    Add PUT /todos

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get('/todos/:id', function (req, res) {
 	var matchedTodo = _.findWhere(todos, {id: todoId});
 
 	if (!matchedTodo)
-		res.status(404).send();
+		return res.status(404).send();
 	else
 		res.json(matchedTodo);
 });
@@ -51,11 +51,35 @@ app.delete('/todos/:id', function (req, res) {
 
 	if (matchedTodo) {
 		todos = _.without(todos, matchedTodo);
-
 		res.json(matchedTodo);
 	}
 	else 
-		res.status(404).send();
+		return res.status(404).send();
+});
+
+// PUT /todos/:id
+app.put('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	if (!matchedTodo)
+		return res.status(404).send();
+
+	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed))
+		validAttributes.completed = body.completed;
+	else if (body.hasOwnProperty('completed'))
+		return res.status(400).send();
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0)
+		validAttributes.description = body.description;
+	else if (body.hasOwnProperty('description'))
+		return res.status(400).send();
+
+	_.extend(matchedTodo, validAttributes);
+	res.json(matchedTodo);
 });
 
 app.listen(PORT, function () {
