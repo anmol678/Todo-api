921e3e7412803d991f227fd098b8ff935807b20e
anmol678 <anmolj678@gmail.com>
2016-12-24 18:39:41 +0530

    Final static todos

--- server.js
+++ server.js
@@ -28,6 +28,12 @@ app.get('/todos', function (req, res) {
 			return res.status(400).send();
 	} 
 
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return ((todo.description).indexOf(queryParams.q) > -1);
+		});
+	}
+
 	res.json(filteredTodos);
 });
 
