e729aedaabb6ab9813cbf6efd698cee8607df0de
anmol678 <anmolj678@gmail.com>
2016-12-27 22:34:39 +0530

    Convert DELETE /todos?:id to sequelize

--- server.js
+++ server.js
@@ -35,36 +35,16 @@ app.get('/todos', function(req, res) {
 		};
 	}
 
-	db.todo.findAll({where: where}).then(function (todos) {
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
 		if (_.isEmpty(todos))
 			res.status(404).send();
 		else
 			res.json(todos);
-	}, function (error) {
+	}, function(error) {
 		res.status(500).send();
 	});
-	// var filteredTodos = todos;
-
-	// if (queryParams.hasOwnProperty('completed')) {
-	// 	if (queryParams.completed === 'true')
-	// 		filteredTodos = _.where(filteredTodos, {
-	// 			completed: true
-	// 		});
-	// 	else if (queryParams.completed === 'false')
-	// 		filteredTodos = _.where(filteredTodos, {
-	// 			completed: false
-	// 		});
-	// 	else
-	// 		return res.status(400).send();
-	// }
-
-	// if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
-	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-	// 	});
-	// }
-
-	// res.json(filteredTodos);
 });
 
 // GET /todos/:id
@@ -95,15 +75,19 @@ app.post('/todos', function(req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 
-	if (matchedTodo) {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	} else
-		return res.status(404).send();
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted) {
+		if (rowsDeleted === 0)
+			res.status(404).send();
+		else
+			res.status(204).send();
+	}, function(error) {
+		res.status(500).json(error);
+	});
 });
 
 // PUT /todos/:id
