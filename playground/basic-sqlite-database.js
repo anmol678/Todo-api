bb762a03ad979cb672607312562f0094d9cc31d4
anmol678 <anmolj678@gmail.com>
2017-01-03 18:32:48 +0530

    Adding todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -19,33 +19,73 @@ var Todo = sequelize.define('todo', {
 	}
 });
 
-sequelize.sync({ force: true }).then(function() {
+var User = sequelize.define('user', {
+	email: Sequelize.STRING
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
+
+sequelize.sync({
+	// force: true
+}).then(function() {
 	console.log('Everything is synced');
-	Todo.create({
-		description: 'Walk the dog',
-		completed: false
-	}).then(function () {
-		return Todo.create({
-			description: 'Pet the dog'
-		});
-	}).then(function () {
-		// return Todo.findById(1);
-		return Todo.findAll({
+
+	User.findById(1).then(function(user) {
+		user.getTodos({
 			where: {
-				description: {
-					$like: '%pet%'
-				}
+				completed: false
 			}
-		});
-	}).then(function (todos) {
-		if (todos) {
-			todos.forEach(function (todo) {
+		}).then(function(todos) {
+			todos.forEach(function(todo) {
 				console.log(todo.toJSON());
 			});
-		}
-		else
-			console.log('No todo found');
-	}).catch(function (error) {
-		console.log(error);
+		});
 	});
+
+	// User.create({
+	// 	email: "anmolj678@gmail.com"
+	// }).then(function() {
+	// 		return Todo.create({
+	// 			description: "clean yard"
+	// 		});
+	// }).then(function(todo) {
+	// 	User.findById(1).then(function(user) {
+	// 		user.addTodo(todo);
+	// 	});
+	// });
 });
+
+
+
+// sequelize.sync({
+// 	force: true
+// }).then(function() {
+// 	console.log('Everything is synced');
+// 	Todo.create({
+// 		description: 'Walk the dog',
+// 		completed: false
+// 	}).then(function() {
+// 		return Todo.create({
+// 			description: 'Pet the dog'
+// 		});
+// 	}).then(function() {
+// 		// return Todo.findById(1);
+// 		return Todo.findAll({
+// 			where: {
+// 				description: {
+// 					$like: '%pet%'
+// 				}
+// 			}
+// 		});
+// 	}).then(function(todos) {
+// 		if (todos) {
+// 			todos.forEach(function(todo) {
+// 				console.log(todo.toJSON());
+// 			});
+// 		} else
+// 			console.log('No todo found');
+// 	}).catch(function(error) {
+// 		console.log(error);
+// 	});
+// });
